750fc9845879917f08bd00f42e8ae26e87c33060
Nahuel Andrada <nahuel@euroasiapay.com>
2025-05-16 19:48:54 -0300

    Creates routeNotFound middleware

--- src/middleware/error-handler.ts
+++ src/middleware/error-handler.ts
@@ -1,8 +1,11 @@
+// Models
+import HttpError from "../models/http-error.ts";
+
 // Types
-import type { ErrorRequestHandler } from "express";
+import type { ErrorRequestHandler, RequestHandler } from "express";
 
 // Constants
-import { ERROR_MESSAGE } from "../globals/constants.ts";
+import { ERROR_MESSAGE, STATUS } from "../globals/constants.ts";
 
 export const errorHandler: ErrorRequestHandler = (error, _req, res, next) => {
   if (res.headersSent) {
@@ -10,4 +13,9 @@ export const errorHandler: ErrorRequestHandler = (error, _req, res, next) => {
   }
   res.status(error.code || 500); // Sets default code if not set
   res.json({ message: error.message || ERROR_MESSAGE.ERROR }); // Sets default message if not set
+};
+
+export const routeNotFound: RequestHandler = (_req, _res, _next) => {
+  const error = new HttpError(ERROR_MESSAGE.ROUTE_NOT_FOUND, STATUS.NOT_FOUND);
+  throw error;
 };
