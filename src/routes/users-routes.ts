dbb769360cb830816a00279eac5bf2bef36d99af
Nahuel Andrada <nahuel@euroasiapay.com>
2025-05-24 03:45:33 -0300

    Adds validators import

--- src/routes/users-routes.ts
+++ src/routes/users-routes.ts
@@ -3,6 +3,12 @@ import express, { Router } from "express";
 // Controllers
 import { getUsers, signup, login } from "../controllers/users-controllers.ts";
 
+// Validators
+import {
+  loginValidator,
+  signupValidator,
+} from "../validators/users-validators.ts";
+
 const usersRoutes: Router = express.Router();
 
 usersRoutes.get("/", getUsers);
